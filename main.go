4d3d220d01fbeaaa36cd756180e49f14425a33f2
Martijn Faber <martijn@fabtronics.nl>
2021-08-23 15:54:05 +0200

    feat(main): capture the screen, calculate the blocksize and number of blocks, and send it to wled

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/Martijn-Faber/Ambilight/config"
+	"github.com/Martijn-Faber/Ambilight/internal/block"
+	"github.com/Martijn-Faber/Ambilight/internal/protocol/wled"
+	"github.com/Martijn-Faber/Ambilight/pkg/screenshot"
+	"github.com/Martijn-Faber/Ambilight/util"
+)
+
 func main() {
+	config, err := config.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: fix ip to config
+	wled.Init("192.168.0.180", config.WledPort)
+	scrWidth, scrHeight, _ := util.GetScreenInfo()
+
+	for {
+		img, err := screenshot.CaptureScreen()
+
+		if err != nil {
+			panic(err)
+		}
 	
+		blkSizeX, blkSizeY := block.CalculateBlockSize(scrWidth, scrHeight, config.NumLedsX, config.NumLedsY)
+		numBlkX, numBlkY := block.CalculateNumBlocks(scrWidth, scrHeight, blkSizeX, blkSizeY)
+	
+		clr := block.GetBlocks(img, blkSizeX, blkSizeY, numBlkX,  numBlkY, scrWidth)
+
+		err = wled.Send(clr)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		time.Sleep(time.Duration(config.RefTimeMs) * time.Millisecond)
+	}
 }
